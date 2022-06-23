34700b0feba2fe0ce99da2326fefa8e5982958cf
lucas <gepeoverload@gmail.com>
2022-06-23 10:43:06 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+if(isset($_GET['page'])){
+    switch($_GET['page']) {
+        case 'classes' :
+            require 'classes.php';
+            break;
+        case 'extending' :
+            require 'extending.php';
+            break;
+        default :
+            echo 'hmmm give me a page sicco';
+    };
+
+
+}
